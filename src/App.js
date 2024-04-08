81498b80cc2ae5270d32a78c1476a1527d865fd6
KumarMritunjay01 <kmritunjay525@gmail.com>
2024-04-08 08:22:41 +0530

    Initial changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Slider from "@material-ui/core/Slider";
+import SelectBox from "./components/SelectBox";
+import ButtonCom from "./components/ButtonCom";
+import ButtonGrpCom from "./components/ButtonGrpCom";
+import CheckBoxCom from "./components/CheckBoxCom";
+import RadiobuttonCom from "./components/RadiobuttonCom";
+import SliderCom from "./components/SliderCom";
+import RangesliderCom from "./components/RangesliderCom";
+import TextFieldCom from "./components/TextFieldCom";
+import SwitchCom from "./components/SwitchCom";
+import BoxCom from "./components/BoxCom";
+import ContainerCom from "./components/ContainerCom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ButtonCom />
+      <ButtonGrpCom />
+      <CheckBoxCom />
+      <RadiobuttonCom />
+      <SliderCom />
+      <RangesliderCom />
+      <SelectBox />
+      <TextFieldCom />
+      <SwitchCom />
+      <BoxCom />
+      <ContainerCom />
     </div>
   );
 }
